c7e0ed9f278e088f10e18359678e0cc8e18c5f98
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 14:45:48 +0900

    Greenになったのでリファクタリングする
    - プロダクトコードとテストコードのリファクタリング(重複や冗長な部分を削除)を行う。Greenの状態を保ったまま行う。
    - あんまりリファクタリングする所はなさそう。強いて言うならテストコードのactualをinline化するぐらい。

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -10,9 +10,7 @@ class FizzBuzzTest {
   void _1を渡すと文字列1を返す() throws Exception {
     // 準備
     FizzBuzz fizzBuzz = new FizzBuzz();
-    // 実行
-    String actual = fizzBuzz.convert(1);
-    // 検証
-    assertEquals("1", actual);
+    // 実行 & 検証
+    assertEquals("1", fizzBuzz.convert(1));
   }
 }
