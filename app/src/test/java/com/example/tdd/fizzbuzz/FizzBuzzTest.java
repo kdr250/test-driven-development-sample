57187d4ba098440c4fdb0bc840c7a66cb084c348
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 16:43:53 +0900

    TODOリストの仕様レベルの言葉でテストを階層構造にグルーピング
    - JUnitだとinner classとNestedでできる。

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 package com.example.tdd.fizzbuzz;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,23 +15,32 @@ class FizzBuzzTest {
     fizzBuzz = new FizzBuzz();
   }
 
-  @Test
-  void _1を渡すと文字列1を返す() throws Exception {
-    assertEquals("1", fizzBuzz.convert(1));
+  @Nested
+  class 数を文字列に変換する {
+    @Test
+    void _1を渡すと文字列1を返す() throws Exception {
+      assertEquals("1", fizzBuzz.convert(1));
+    }
+
+    @Test
+    void _2を渡すと文字列2を返す() throws Exception {
+      assertEquals("2", fizzBuzz.convert(2));
+    }
   }
 
-  @Test
-  void _2を渡すと文字列2を返す() throws Exception {
-    assertEquals("2", fizzBuzz.convert(2));
+  @Nested
+  class _3の倍数の時は数の代わりにFizzに変換する {
+    @Test
+    void _3を渡すと文字列Fizzを返す() throws Exception {
+      assertEquals("Fizz", fizzBuzz.convert(3));
+    }
   }
 
-  @Test
-  void _3を渡すと文字列Fizzを返す() throws Exception {
-    assertEquals("Fizz", fizzBuzz.convert(3));
-  }
-
-  @Test
-  void _5を渡すと文字列Buzzを返す() throws Exception {
-    assertEquals("Buzz", fizzBuzz.convert(5));
+  @Nested
+  class _5の倍数の時は数の代わりにBuzzに変換する {
+    @Test
+    void _5を渡すと文字列Buzzを返す() throws Exception {
+      assertEquals("Buzz", fizzBuzz.convert(5));
+    }
   }
 }
