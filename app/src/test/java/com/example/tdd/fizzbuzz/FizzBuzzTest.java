a63a6217d3e9d2f588fa06122cefef25b43f575b
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 13:53:32 +0900

    FizzBuzzTestクラスを作成
    最初にセットアップやテスティングフレームワークなどがちゃんと動作するかの確認のため、必ず失敗するテストを書く。失敗メッセージに「これは失敗するはず」と表示されるかを確認する。

---
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.example.tdd.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FizzBuzzTest {
+
+  @Test
+  void test() {
+    fail("これは失敗するはず");
+  }
+}
