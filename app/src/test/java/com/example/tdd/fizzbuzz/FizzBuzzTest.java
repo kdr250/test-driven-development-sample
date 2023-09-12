d9d4231e44154be931868de32cb87b779f9e8efe
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 14:55:50 +0900

    2を渡すと文字列2を返すテスト追加
    - 三角測量として2を渡すと文字列2を返すテスト追加
    - Unitテストにおいて、アサーションは1テストメソッドに1つにする(One Assertion Per Testの原則)
    - なぜかというと、1つのメソッド内に複数のアサーションを書いてしまうと、どのアサーションでテスト失敗したのか探すところからデバッグし始めないといけないため。アサーションルーレットなどと呼ばれるアンチパターン。

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -13,4 +13,12 @@ class FizzBuzzTest {
     // 実行 & 検証
     assertEquals("1", fizzBuzz.convert(1));
   }
+
+  @Test
+  void _2を渡すと文字列2を返す() throws Exception {
+    // 準備
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    // 実行 & 検証
+    assertEquals("2", fizzBuzz.convert(2));
+  }
 }
