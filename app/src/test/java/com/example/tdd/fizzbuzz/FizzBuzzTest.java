5b1c4ccc65f2b8d07b356cd68b245bacd3d4d182
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 16:47:49 +0900

    テストコードの構造が仕様の構造を表すよう修正
    - テストは動く仕様書である、というのは文字通りそうする。

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,11 +1,13 @@
 package com.example.tdd.fizzbuzz;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("FizzBuzz数列と変換規則を扱うFizzBuzzクラス")
 class FizzBuzzTest {
 
   private FizzBuzz fizzBuzz;
@@ -16,31 +18,35 @@ class FizzBuzzTest {
   }
 
   @Nested
-  class 数を文字列に変換する {
-    @Test
-    void _1を渡すと文字列1を返す() throws Exception {
-      assertEquals("1", fizzBuzz.convert(1));
+  class convertメソッドは数を文字列に変換する {
+
+    @Nested
+    class _3の倍数の時は数の代わりにFizzに変換する {
+      @Test
+      void _3を渡すと文字列Fizzを返す() throws Exception {
+        assertEquals("Fizz", fizzBuzz.convert(3));
+      }
     }
 
-    @Test
-    void _2を渡すと文字列2を返す() throws Exception {
-      assertEquals("2", fizzBuzz.convert(2));
+    @Nested
+    class _5の倍数の時は数の代わりにBuzzに変換する {
+      @Test
+      void _5を渡すと文字列Buzzを返す() throws Exception {
+        assertEquals("Buzz", fizzBuzz.convert(5));
+      }
     }
-  }
 
-  @Nested
-  class _3の倍数の時は数の代わりにFizzに変換する {
-    @Test
-    void _3を渡すと文字列Fizzを返す() throws Exception {
-      assertEquals("Fizz", fizzBuzz.convert(3));
-    }
-  }
+    @Nested
+    class その他の数の場合は数を文字列に変換する {
+      @Test
+      void _1を渡すと文字列1を返す() throws Exception {
+        assertEquals("1", fizzBuzz.convert(1));
+      }
 
-  @Nested
-  class _5の倍数の時は数の代わりにBuzzに変換する {
-    @Test
-    void _5を渡すと文字列Buzzを返す() throws Exception {
-      assertEquals("Buzz", fizzBuzz.convert(5));
+      @Test
+      void _2を渡すと文字列2を返す() throws Exception {
+        assertEquals("2", fizzBuzz.convert(2));
+      }
     }
   }
 }
