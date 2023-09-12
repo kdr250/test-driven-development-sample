e7e6cf3dffe0e24090e850e49010a520dc8cd8a4
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 16:22:38 +0900

    テストもリファクタリンッグ
    - 準備フェーズが重複しているので、重複を排除できそう。
    - JUnitだとBeforeEachを使えば良い
    - リファクタリング中もなるべくコンパイルエラーにならないように変数を移動させたりすると、より確実になる。
    - 先にprivate FizzBuzz fizzBuzzを宣言した後で、それぞれの変数を前準備に移動させる、みたいな感じ。

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,32 +1,31 @@
 package com.example.tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
+  private FizzBuzz fizzBuzz;
+
+  @BeforeEach
+  void 前準備() {
+    fizzBuzz = new FizzBuzz();
+  }
+
   @Test
   void _1を渡すと文字列1を返す() throws Exception {
-    // 準備
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    // 実行 & 検証
     assertEquals("1", fizzBuzz.convert(1));
   }
 
   @Test
   void _2を渡すと文字列2を返す() throws Exception {
-    // 準備
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    // 実行 & 検証
     assertEquals("2", fizzBuzz.convert(2));
   }
 
   @Test
   void _3を渡すと文字列Fizzを返す() throws Exception {
-    // 準備
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    // 実行 & 検証
     assertEquals("Fizz", fizzBuzz.convert(3));
   }
 }
