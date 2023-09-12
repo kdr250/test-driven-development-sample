e0197d9b17838fe8f159b83e2557c92203168ce0
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 14:05:56 +0900

    最初に数を文字列に変換するテストを作ってみる
    - メソッド名は(オフショア開発やOSSでもない限り)母語つまり日本語にするのがオススメ。テストは仕様を表現するものでもあるのだから。
    - テストは準備/実行/検証の順番で書くのが基本で、3Aパターンと呼ばれる。振る舞い駆動開発(BDD)だと用語や意図が変わってくる。
    - 最初にアサーションから書く。適当にズラズラ書いていくとアサーションが増殖していき何をテストしたかったのか分からなくなるから。
    - 「数を文字列に変換する」をどうやってコードにするかで手が止まってしまった。できると思っていたのに考えられていなかった、というのが開発の前段階にスライドできればプロジェクトのリスクは軽減できる。
    - このあとTODOリストに戻ってタスクを見直してみる

--- app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/com/example/tdd/fizzbuzz/FizzBuzzTest.java
@@ -2,12 +2,15 @@ package com.example.tdd.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
   @Test
-  void test() {
-    fail("これは失敗するはず");
+  void 数を文字列に変換する() throws Exception {
+    // 準備
+    // 実行
+    // 検証
+    assertEquals(expected, actual)
   }
 }
