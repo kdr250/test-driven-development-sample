34ba067f42ca31c8848cc856860382f23f4f57dc
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-09-12 16:25:59 +0900

    仮実装の状態から本来あるべき実装へ
    - テストコードのテストはさっき済ませて信頼できる状態
    - なので仮実装の状態からジャンプして実装にいっても良い。
    - 仮実装から必ず三角測量を経由しなければならないという訳ではない。臨機応変に。

--- app/src/main/java/com/example/tdd/fizzbuzz/FizzBuzz.java
+++ app/src/main/java/com/example/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.example.tdd.fizzbuzz;
 
 public class FizzBuzz {
   public String convert(int number) {
-    if (number == 3) {
+    if (number % 3 == 0) {
       return "Fizz";
     }
     return String.valueOf(number);
